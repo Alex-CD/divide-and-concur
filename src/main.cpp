490a7d7d39abfb214523f3ef3ee8e3b282163501
Alexander Clarke <alexandermclarke@gmail.com>
2018-11-08 20:07:13 +0000

    Add dependencies, add POC rendering

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,58 @@
 // Created by Alex on 18/10/2018.
 //
 
+#include <stdint.h>
 #include <string>
 #include <iostream>
+#include <glad/glad.h>
+
+#include <GLFW/glfw3.h>
+
+
 
 using namespace std;
 
 int main() {
-    cout << "Welcome to divide and concur, a concurrency-based game environment for the modern era." << endl;
 
-    bool continueLoop = true;
+  GLFWwindow* window;
 
-    while (continueLoop){
-      cout << "Type in the name of a game to play:";
+  /* Initialize the library */
+  if (!glfwInit())
+    return -1;
 
-      //Finds list of games
-      //Instantiates game in new thread
-    }
+  /* Create a windowed mode window and its OpenGL context */
+  window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+  if (!window)
+  {
+    glfwTerminate();
+    return -1;
+  }
+
+  /* Make the window's context current */
+  glfwMakeContextCurrent(window);
+
+
+  if(!gladLoadGL()) {
+    printf("Something went wrong!\n");
+    exit(-1);
+  }
+  printf("OpenGL %d.%d\n", GLVersion.major, GLVersion.minor);
+
+
+  /* Loop until the user closes the window */
+  while (!glfwWindowShouldClose(window))
+  {
+    /* Render here */
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    /* Swap front and back buffers */
+    glfwSwapBuffers(window);
+
+    /* Poll for and process events */
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return 0;
+
 }
