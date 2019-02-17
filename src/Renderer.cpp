ec138da936e87e10a98c7d7d02bd16b22df42d47
Alexander Clarke <alexander_m_clarke@live.co.uk>
2019-02-17 12:05:03 +0000

    Get rendering working - hello triangle

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -156,7 +156,6 @@ void Renderer::initGL(){
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
   glEnableVertexAttribArray(0);
 
-  glUseProgram(this->shaderProgram);
 
 }
 
@@ -220,20 +219,24 @@ void Renderer::renderLoop() {
 
 
   float vertices[] = {
-      -0.5f, -0.5f, 0.0f,
-      0.5f, -0.5f, 0.0f,
-      0.0f,  0.5f, 0.0f
+      -1.0f, -1.0f, 0.0f,
+      1.0f, 1.0f, 0.0f,
+      -1.0f,  1.0f, 0.0f
   };
 
 
+  glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+  glEnableVertexAttribArray(0);
+
+
+  glUseProgram(this->shaderProgram);
+
   while (!glfwWindowShouldClose(this->window) && !*this->isTerminating) {
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glClear(GL_COLOR_BUFFER_BIT);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
-
-
-
-
-
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
 
     /* Swap front and back buffers */
