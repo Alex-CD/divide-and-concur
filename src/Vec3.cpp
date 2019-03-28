25edcf703e335e47238d7a87b92a94f4196c0e28
Alexander Clarke <alexander_m_clarke@live.co.uk>
2019-03-28 12:38:51 +0000

    Add Vec3 struct for code refactoring

---
+++ src/Vec3.cpp
@@ -0,0 +1,12 @@
+//
+// Created by alex on 28/03/19.
+//
+
+#include "Vec3.h"
+
+
+Vec3::Vec3(float x, float y, float z) {
+  this->x = x;
+  this->y = y;
+  this->z = z;
+}
---
+++ src/Vec3.h
@@ -0,0 +1,15 @@
+//
+// Created by alex on 28/03/19.
+//
+
+#ifndef DAC_VEC3_H
+#define DAC_VEC3_H
+
+struct Vec3 {
+  float x;
+  float y;
+  float z;
+  Vec3(float x, float y, float z);
+};
+
+#endif //DAC_VEC3_H
