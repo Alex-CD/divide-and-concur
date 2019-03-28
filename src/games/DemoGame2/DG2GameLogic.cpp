c7aa15760bd93aed55d58a10be2d0408e37060f2
Alexander Clarke <alexander_m_clarke@live.co.uk>
2019-03-28 16:51:21 +0000

    Adjust demo game 2

--- src/games/DemoGame2/DG2GameLogic.cpp
+++ src/games/DemoGame2/DG2GameLogic.cpp
@@ -20,16 +20,28 @@ void DG2GameLogic::logicLoop(){
   Object *currentObject;
 
   while(!*this->isTerminating){
-    if(this->mouse->leftButtonIsDown){
-      if(!buttonStillDown) {
-        buttonStillDown = true;
-        currentObject = objects->addObject(new Vector(Vec3(100.0f, 100.0f, 100.0f), ""));
-        currentObject->setPos(this->mouse->xPos, this->mouse->yPos, 0);
+    if(this->mouse->leftButtonIsDown) {
+      currentObject = objects->addObject(new Vector(Vec3(100.0f, 100.0f, 100.0f), ""));
+      ((Vector *) currentObject)->setColour(Vec3(1.0f, 1.0f, 1.0f), Vec3(1.0f, 1.0f, 1.0f), Vec3(1.0f, 1.0f, 1.0f));
+      currentObject->setPos(this->mouse->xPos, this->mouse->yPos, 0);
+
+      clock_t clicktime = clock();
+
+      int TICKS_COEF = 80000;
+
+      while(this->mouse->leftButtonIsDown) {
+
+        double scale = 1 + ((clock() - clicktime)/(TICKS_COEF)*0.05);
+        currentObject->setTransform(scale, scale, 0);
+        ((Vector *)currentObject)->setColour(Vec3(1.0f,scale*0.1f,scale*0.01f),Vec3(1.0f, 0.5f, 1.0f),Vec3(0.0f,scale*1.0f,scale*1.0f));
+        currentObject->setPos(Vec3(this->mouse->xPos, this->mouse->yPos, 0));
       }
     }
-    else {
-      buttonStillDown = false;
+    else if(this->mouse->middleButtonIsDown){
+      currentObject = objects->addObject(new Vector(Vec3(100.0f, 100.0f, 100.0f), ""));
+      ((Vector *)currentObject)->setColour(Vec3(0.5f, 0.0f, 0.0f),Vec3(1.0f, 0.5f, 1.0f),Vec3());
+      currentObject->setPos(Vec3(this->mouse->xPos, this->mouse->yPos, 0));
     }
   }
-  }
+}
 
